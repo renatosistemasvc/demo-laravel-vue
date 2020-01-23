549c1061d01a6028fc8211e7b4d9d5f238cbbe81
Renato Souza de Oliveira <renatosistemas.vc@gmail.com>
2020-01-23 01:11:27 -0300

    build

---
+++ demo_web/prod/dist/static/js/chunk-2d0dddfd.d4f71946.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0dddfd"],{"82f3":function(n,t,e){"use strict";e.r(t);var o=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"main"})},s=[],c={methods:{metodo:function(){}},components:{}},d=c,u=e("2877"),a=Object(u["a"])(d,o,s,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0dddfd.d4f71946.js.map
